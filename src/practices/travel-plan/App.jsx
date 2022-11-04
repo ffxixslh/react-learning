70447f7535470b549a169f452d90d72afa686a88
luohao <1223048059@qq.com>
2022-11-04 16:27:44 +0800

    更新目录

--- src/practices/travel-plan/App.jsx
+++ src/practices/travel-plan/App.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react'
-import { initialTravelPlan } from './assets/places'
+import { initialTravelPlan } from '../../assets/places'
 import './App.css'
 
 function PlaceTree ({ id, parentId, placesId, onComplete }) {
